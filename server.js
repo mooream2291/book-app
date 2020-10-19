d667738ec4298f5e2e429ca132b9254ead3b2101
Ashley Moore <mooream2291@gmail.com>
2020-10-19 15:57:22 -0400

    setup

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express');
+const superagent = require('superagent');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended}))
+app.set('view engine', 'ejs');
+app.post('/searches', createSearch);
+function createSearch(req, res) {
+let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+console.log('request body:', req.body);
+console.log('form data:', req.body.search);
+if (req.body.search[1] === 'title') {url += `intitle:req.body.search[0]`}
+if (req.body.search[1] === 'author') {url += `intitle:req.body.search[0]`}
+}
+superagent.get(url)
+.then(data => res.json(data));
