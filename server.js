1a49a307ec3d9dca8ef12a321b6ce015975c24ae
Ashley Moore <mooream2291@gmail.com>
2020-10-19 21:40:46 -0400

    my brain

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('./public'));
 //loading css after page load//
 app.set('view engine', 'ejs');
-app.post('/searches', createSearch);
+app.post('/searches/new.ejs', createSearch);
 
 function createSearch(req, res) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
@@ -18,31 +18,33 @@ function createSearch(req, res) {
   if (req.body.search[1] === 'title') url += `intitle:req.body.search[0]`;
   if (req.body.search[1] === 'author') url += `intitle:req.body.search[0]`;
 
+
   superagent.get(url)
-    .then(data => res.json(data));
+    .then(data => {
+      console.log('google books data:', data);
+      res.json(data.text);
+
+      app.get('/hello', (req, res) => {
+        res.render('pages/index.ejs');
+      });
+
+      function Book(data){
+        this.author = data.title ? data.title: 'Unavailable';
+        this.title = data.authors ? data.authors: 'Unavailable';
+        this.description = data.description ? data.description: 'Unavailable';
+        let coverArt = data.imageLinks.thumbnail ? data.imageLinks.thumbnail: 'https://i.imgur.com/J5LVHEL.jpg';
+
+        if (coverArt.slice(0, 5) !== 'https') {
+          coverArt = 'https' + coverArt.slice(4,coverArt.length);
+        }
+        this.coverArt = coverArt;
+
+        app.get('/searches/new', (request, response) => {
+          response.render('pages/searches/new.ejs');
+        });
+      }
+      app.listen(PORT, ()=> {
+        console.log(`listening on ${PORT}`);
+      });
+    });
 }
-app.listen(PORT, ()=> {
-  console.log(`listening on ${PORT}`);
-});
-
-app.get('/hello', (request, response) => {
-  response.render('pages/index.ejs');
-}
-);
-
-app.get('/searches/new', (request, response) => {
-  response.render('pages/searches/new.ejs');
-});
-
-// function Book {
-//   this.title = 
-//   this.author = 
-// };
-
-// const greeting = 'hello' + person.object ? person.name: 'person' 
-
-// this.bookArt = obj.art ? obj.art: '/default.png'
-
-
-
-
