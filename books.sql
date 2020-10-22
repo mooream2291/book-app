54632416a5bd9b7acfe0d4b5ada63497c91dd421
DORA BLAKEY DIANA <DORANICCOLE@YAHOO.COM>
2020-10-22 00:21:23 -0600

    changed syntax formatted tabs added partials

--- books.sql
+++ books.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS book_table;
 
-CREATE TABLE IF NOT EXISTS book_table (
+CREATE TABLE book_table (
   id SERIAL PRIMARY KEY,
   author VARCHAR(255),
   title VARCHAR(255),
@@ -10,11 +10,15 @@ CREATE TABLE IF NOT EXISTS book_table (
   description TEXT
 );
 
-INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
+INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) 
+  VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
 
-INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
+INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) 
+  VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
 
-INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
+INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) 
+  VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
 
-INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
+INSERT INTO book_table (author, title, image_url, description, bookshelf, isbn) 
+  VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
 
