38b2d2f1a0681e53f0d679f5a52f66653c7706c9
Ashley Moore <mooream2291@gmail.com>
2020-10-21 20:28:38 -0400

    x

--- books.sql
+++ books.sql
@@ -1,12 +1,19 @@
-DROP TABLE IF EXISTS book_tables;
+DROP TABLE IF EXISTS book_table;
 
 CREATE TABLE IF NOT EXISTS book_table (
   id SERIAL PRIMARY KEY,
   author VARCHAR(255),
   title VARCHAR(255),
-  isbn NUMBER,
+  isbn NUMERIC,
+  bookShelf VARCHAR,
   image_url VARCHAR,
   description VARCHAR
-)
+);
 
-INSERT INTO book_table (authors, title, image_url, description, bookshelf) VALUES (‘some author’, ‘some title’, ‘some image’, ‘some descript’, ‘some bookshelf’);
+INSERT INTO book_table (author, title, image_url, description, bookShelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
+
+INSERT INTO book_table (author, title, image_url, description, bookShelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
+
+INSERT INTO book_table (author, title, image_url, description, bookShelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
+
+INSERT INTO book_table (author, title, image_url, description, bookShelf, isbn) VALUES ('some author', 'some title', 'some image', 'some descript', 'some bookshelf', 456894576);
--- server.js
+++ server.js
@@ -23,28 +23,40 @@ app.get('/index', (req, res) => {
     });
 });
 //pulls everything in from API into database when user puts in search info//
-app.get('/books: id', {})
-app.post('/getNewBook');
-  const sql = 'SELECT * FROM '
+//when there is a piece of data after books (denoted by colon) save this piece of information using request.params//
+app.get('/books/:id', (req, res) => {
+  console.log('trying to get single book');
+//request.params.id (id comes from key in books.sql (which is our database) id is a unique value given to each object by postgres node)
+  const id = req.params.id;
+  const sql = `SELECT * FROM book_table WHERE id=${id}`;
+  console.log(sql);
+  client.query(sql)
+    .then(results => {
+      //.rows is an array of the rwos that are a result of my sql statement postgres puts objects intoa  table and this is asking for the rows the table//
+      res.render('pages/books/detail', {book: results.rows[0]});
+    })
+    .catch(error => console.log(error));
+});
+// app.post('/getNewBook');
 //app.post to add to database from select this book button on wireframe//
 //do a client.query call the res.render will display on page.//
 
 function createSearch(req, res) {
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  console.log('request body:', req.body);
-  console.log('form data:', req.body.search);
+  // console.log('request body:', req.body);
+  // console.log('form data:', req.body.search);
   if (req.body.search[1] === 'title') url += `intitle:${req.body.search[0]}`;
   if (req.body.search[1] === 'author') url += `inauthor:${req.body.search[0]}`;
 
   console.log(url);
-//superagent is just a tool to hit APIs//
+  //superagent is just a tool to hit APIs//
   superagent.get(url)
     .then(data => {
       console.log('google books data:', data.body.items);
       const newBookArr = data.body.items.map(book => {
         return new Book(book);
       });
-//whatever I for each through is what i want to render in my route//
+      //whatever I for each through is what i want to render in my route//
       res.render('pages/searches/show', {results: newBookArr});
     })
     .catch((error) => {
@@ -57,8 +69,8 @@ function Book(data){
   this.author = data.volumeInfo.authors ? data.volumeInfo.authors: 'Unavailable';
   this.title = data.volumeInfo.title ? data.volumeInfo.title: 'Unavailable';
   this.description = data.volumeInfo.description ? data.volumeInfo.description: 'Unavailable';
-  this.isbn = data.volumeInfo.GETFROMAPI ? data.volumeInfo.GETINFOFROMAPI: 'Unavailable';
-  this.bookShelf = data.volumeInfo.GETFROMAPI ? data.volumeInfo.GETINFOFROMAPI: 'Unavailable';
+  this.isbn = data.volumeInfo. industryIdentifiers ? data.volumeInfo. industryIdentifiers: 'Unavailable';
+  this.bookShelf = data.volumeInfo.categories ? data.volumeInfo.categories: 'Unavailable';
   let coverArt = data.volumeInfo.imageLinks ? data.volumeInfo.imageLinks.thumbnail: 'https://i.imgur.com/J5LVHEL.jpg';
 
   if (coverArt.slice(0, 5) !== 'https') {
@@ -70,6 +82,11 @@ app.get('/searches/new', (req, res) => {
   res.render('pages/searches/new.ejs');
 //.get, or .post, that string is the URL, it goes into the URL bar to determine what to do, only use specific file when using render method
 });
-app.listen(PORT, ()=> {
-  console.log(`listening on ${PORT}`);
-});
+//connects database before express takes API requests//
+client.connect()
+  .then(() => {
+    app.listen(PORT, ()=> {
+      console.log(`listening on ${PORT}`);
+    });
+  });
+
