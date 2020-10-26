56cecbbcba21866c6e0979e9ec5228c484c0e4a3
Ashley Moore <mooream2291@gmail.com>
2020-10-25 22:59:17 -0400

    x

--- server.js
+++ server.js
@@ -20,16 +20,22 @@ app.set('view engine', 'ejs');
 //queries the API
 app.post('/searches', createSearch);
 //represents URL that the user is going to//
-app.get('/index', (req, res) => {
-  const sql = 'SELECT * FROM book_table;';
-  client.query(sql)
-    .then(results => {
-      res.render('pages/index', {'book_table': results.rows});
-    });
-});
+app.get('/index');
+
 //pulls everything in from API into database when user puts in search info//
 //when there is a piece of data after books (denoted by colon) save this piece of information using request.params//
-app.get('/books/:id', (req, res) => { //for ticket 4 change arrow functions to a function expression (46 is an example)//
+app.get('/books/:id', getOneBook);
+
+app.post('/addnewbook', saveBook);
+
+app.get('/searches/new', getUrl);//homepage
+
+app.put('update/books/:id', updateBook);
+
+//TODO: ADD PUT route
+//TODO: ADD DELETE route
+
+function getOneBook (req, res) { //for ticket 4 change arrow functions to a function expression (46 is an example)//
   console.log('trying to get single book');
 //request.params.id (id comes from key in books.sql (which is our database) id is a unique value given to each object by postgres node)
   const id = req.params.id;
@@ -41,8 +47,8 @@ app.get('/books/:id', (req, res) => { //for ticket 4 change arrow functions to a
       res.render('pages/books/detail', {book: results.rows[0]});
     })
     .catch(error => console.log(error));
-});
-app.post('/addnewbook', saveBook);
+};
+// eslint-disable-next-line no-redeclare
 function saveBook(req, res) {
 //INSERT SQL statement//
 //anything posting to form data needs to be req.body
@@ -56,6 +62,15 @@ function saveBook(req, res) {
       //response: response from either database or API is sent to the user//
     });
 //render to detail//
+}
+function updateBook(req, res) {
+  let { image_url, title, author, description, isbn, bookshelf } = req.body;
+  let SQL = `UPDATE book_table SET image_url=$1, title=$2, author=$3, description=$4, isbn=$5, bookshelf=$6 WHERE id=$7`;//the id does not live on the request body, it lives int he request.params (so the url)
+  let values = [image_url, title, author, description, isbn, bookshelf, req.params.id];
+
+  client.query(SQL, values)
+    .then(res.redirect(`/books/${req.params.books_id}`))
+    .catch(err => console.error(err));
 }
 //app.post to add to database from select this book button on wireframe//
 //do a client.query call the res.render will display on page.//
@@ -97,10 +112,11 @@ function Book(data){
   }
   this.coverArt = coverArt;
 }
-app.get('/searches/new', (req, res) => {
+
+function getUrl (req, res) {
   res.render('pages/searches/new.ejs');
 //.get, or .post, that string is the URL, it goes into the URL bar to determine what to do, only use specific file when using render method
-});
+}
 //connects database before express takes API requests//
 client.connect()
   .then(() => {
